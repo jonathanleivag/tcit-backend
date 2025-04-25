6391fe5db172c0df6984409a8a7cef162fe9e983
Jonathanleivag <email@jonathanleivag.cl>
2025-04-25 04:04:03 -0400

    feat: enable CORS middleware for the application

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { getEnv } from './utils/env.util'
 import { ENV } from './enum'
 import PostRouter from './routers/post.router'
 import helmet from 'helmet'
+import cors from 'cors'
 
 const app = express()
 const port = getEnv(ENV.PORT)
 
 app.use(express.json())
 app.use(helmet())
+app.use(cors())
 
 app.get('/', (_req, res) => {
   res.send('Hello World')
