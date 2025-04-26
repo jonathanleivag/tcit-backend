b65ebd93781c35ef027dcb246b584b564220b9f3
Jonathanleivag <email@jonathanleivag.cl>
2025-04-25 21:24:28 -0400

    refactor: remove unused post service methods and clean up router

--- src/routers/post.router.ts
+++ src/routers/post.router.ts
@@ -1,12 +1,6 @@
 import { Request, Response, Router } from 'express'
 import { Post, PostReqBody, Res } from '../type'
-import {
-  createPost,
-  deletePost,
-  getAllPosts,
-  getPostById,
-  updatePost
-} from '../services/post.service'
+import { createPost, deletePost, getAllPosts } from '../services/post.service'
 
 const router = Router()
 
@@ -25,32 +19,6 @@ router.get('/', async (_req, res: Response<Res<Post[]>>) => {
   const getAll = await getAllPosts()
   res.status(getAll.status).json(getAll)
 })
-
-/**
- * @openapi
- * /api/post/{id}:
- *   get:
- *     tags:
- *       - POST
- *     description: Obtener un post por ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID del post
- *     responses:
- *       200:
- *         description: Post encontrado
- */
-router.get(
-  '/:id',
-  async (req: Request<{ id: string }>, res: Response<Res<Post>>) => {
-    const getOne = await getPostById(Number(req.params.id))
-    res.status(getOne.status).json(getOne)
-  }
-)
 
 /**
  * @openapi
@@ -74,41 +42,6 @@ router.post(
   async (req: Request<{}, {}, PostReqBody>, res: Response<Res<Post>>) => {
     const create = await createPost(req.body)
     res.status(create.status).json(create)
-  }
-)
-
-/**
- * @openapi
- * /api/post/{id}:
- *   put:
- *     tags:
- *       - POST
- *     description: Actualizar un post existente
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: integer
- *         description: ID del post
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/PostReqBody'
- *     responses:
- *       200:
- *         description: Post actualizado correctamente
- */
-router.put(
-  '/:id',
-  async (
-    req: Request<{ id: string }, {}, PostReqBody>,
-    res: Response<Res<Post>>
-  ) => {
-    const update = await updatePost(Number(req.params.id), req.body)
-    res.status(update.status).json(update)
   }
 )
 
